b5c8960b4e789d8c2d1d5017f56decf096437a71
MNRainDrop <79126834+MNRainDrop@users.noreply.github.com>
2023-03-04 04:40:16 -0800

    Cooking something
    Trying to figure out how to make multiple cards show in a row

--- javascript/poker.js
+++ javascript/poker.js
@@ -16,3 +16,45 @@ const CARD_VALUE_MAP = {
     "A" : 14
 }
 
+const communityDeckElement = document.querySelector(".community-deck")
+const communityCardsElement = document.querySelector(".community-cards")
+const playerDeckElement = document.querySelector(".player")
+const dealerDeckElement = document.querySelector(".dealer")
+
+let communityDeck
+let communityCards
+let playerDeck
+let dealerDeck
+
+startGame()
+
+function startGame() {
+    communityDeck = new Deck()
+    communityDeck.shuffle()
+
+    let tempPlayerDeck = []
+    let tempDealerDeck = []
+    let tempCommunityCards = []
+
+    for (let i = 0; i < 2; i++) {
+        tempPlayerDeck.push(communityDeck.pop())
+        tempDealerDeck.push(communityDeck.pop())
+    }
+
+    playerDeck = new Deck(tempPlayerDeck)
+    dealerDeck = new Deck(tempDealerDeck)
+
+    for (let i = 0; i < 5; i++) {
+        tempCommunityCards.push(communityDeck.pop())
+    }
+
+    communityCards = new Deck(tempCommunityCards)
+
+    playRound()
+}
+
+function playRound() {
+    for (let i = 0; i < playerDeck.numOfCards; i++) {
+        playerDeckElement.appendChild(playerDeck.cards[i].getHTML())
+    }
+}
